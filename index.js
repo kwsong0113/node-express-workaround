bafb4bffd0826807048d4081ba955421ff2f796c
Kiwhan Song <kwsong0113@gmail.com>
2023-09-25 18:02:36 -0400

    worker threads

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
+const { Worker, isMainThread, parentPort } = require("worker_threads");
 let express = require("express");
 let app = express();
 
-let i = 0;
-const LOOP_NUM = 9999999999999999999999999;
+if (isMainThread) {
+  let i = 0;
 
-function respond(res, delay) {
-  setTimeout(function () {
-    res.send("Hello " + i++);
-  }, delay);
-}
-
-function compute(i) {
-  // do some computation with i
-}
-
-function loopChunk(start) {
-  if (start > LOOP_NUM) {
-    respond(res, 0);
-    return;
-  } else {
-    end = Math.min(start + 10, LOOP_NUM);
-    for (let i = start; i < end; i++) {
-      compute(i);
-    }
+  function respond(res, delay) {
+    setTimeout(function () {
+      res.send("Hello " + i++);
+    }, delay);
   }
-  setImmediate(() => loopChunk(end + 1));
+
+  app.get("/fast", function (req, res) {
+    respond(res, 0);
+  });
+
+  app.get("/slow1", function (req, res) {
+    const worker = new Worker(__filename);
+    worker.on("message", () => respond(res, 0));
+  });
+
+  app.get("/slow2", function (reg, res) {
+    respond(res, 2147483647); // max signed int value
+  });
+
+  app.listen(3000);
+} else {
+  const LOOP_NUM = 9999999999999999999999999;
+  function compute(i) {
+    // do some computation with i
+  }
+
+  for (let i = 0; i < LOOP_NUM; i++) {
+    compute(i);
+  }
+  parentPort.postMessage("computation completed");
 }
-
-app.get("/fast", function (req, res) {
-  respond(res, 0);
-});
-
-app.get("/slow1", function (req, res) {
-  loopChunk(0);
-});
-
-app.get("/slow2", function (reg, res) {
-  respond(res, 2147483647); // max signed int value
-});
-
-app.listen(3000);
-
-console.log("Listening on port 3000...");
